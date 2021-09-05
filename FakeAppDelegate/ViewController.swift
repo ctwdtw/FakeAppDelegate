af639c79c5f4b55c59971fb40dbaf018d0182a6a
ctwdtw <ctwdtw@gmail.com>
2021-09-05 11:41:08 +0800

    first commit

---
+++ FakeAppDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeAppDelegate
+//
+//  Created by Paul Lee on 2021/9/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
